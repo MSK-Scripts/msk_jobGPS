00bac7ba4079f75147b1e3d59bd3fac4bbe3815e
Musiker15 <info@musiker15.de>
2026-08-02 09:30:38 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -379,14 +379,12 @@ GithubUpdater = function()
 
     if Config.VersionChecker then
         PerformHttpRequest('https://raw.githubusercontent.com/MSK-Scripts/msk_jobGPS/main/VERSION', function(Error, NewestVersion, Header)
-            print("###############################")
             if CurrentVersion == NewestVersion then
                 print(resourceName .. '^2 ✓ Resource is Up to Date^0 - ^5Current Version: ^2' .. CurrentVersion .. '^0')
             elseif CurrentVersion ~= NewestVersion then
                 print(resourceName .. '^1 ✗ Resource Outdated. Please Update!^0 - ^5Current Version: ^1' .. CurrentVersion .. '^0')
                 print('^5Newest Version: ^2' .. NewestVersion .. '^0 - ^6Download here:^9 https://github.com/MSK-Scripts/msk_jobGPS/releases/tag/v'.. NewestVersion .. '^0')
             end
-            print("###############################")
         end)
     else
         print(resourceName .. '^2 ✓ Resource loaded^0')
